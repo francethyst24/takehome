b832bd6b0b7c944d1e672bb3f2cdeed3c6562e48
floydferd <francethyst24@gmail.com>
2022-10-28 12:21:34 +0800

    App Setup
    - Routing (urls, views.index, index.html)
    - Bootstrap CSS-JS

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.conf import settings
+import requests
 
 # Create your views here.
+
+
+def index(request):
+    context = dict()
+    return render(request, 'weather/index.html', context)
